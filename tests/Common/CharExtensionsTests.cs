3d8c3bc22051b5c182ac5ab29a397de912e3b582
Mark Visschers <mark.visschers@connecthing.tech>
2021-07-05 11:42:11 +0200

    Added tests

--- tests/Common/CharExtensionsTests.cs
+++ tests/Common/CharExtensionsTests.cs
@@ -1,6 +1,5 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using FluentAssertions;
-using MatrTech.Utilities.Extensions.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace MatrTech.Utilities.Extensions.Common.UnitTests
 {
@@ -64,6 +63,58 @@ namespace MatrTech.Utilities.Extensions.Common.UnitTests
         {
             var isAlphaNumeric = input.IsLetter();
             isAlphaNumeric.Should().BeFalse();
+        }
+
+        [TestMethod]
+        [DataRow(0x0041, "A")]
+        [DataRow(0x0042, "B")]
+        [DataRow(0x0043, "C")]
+        [DataRow(0x0061, "a")]
+        [DataRow(0x0062, "b")]
+        [DataRow(0x0063, "c")]
+        [DataRow(0x0030, "0")]
+        [DataRow(0x0031, "1")]
+        [DataRow(0x0032, "2")]
+        public void ConvertFromUtf32_SomeUtf32Value_ShouldBeExpectedResult(int utf32, string expectedResult)
+        {
+            utf32.ConvertFromUtf32().Should().Be(expectedResult);
+        }
+
+        [TestMethod]
+        [DataRow('A', 0x0041)]
+        [DataRow('B', 0x0042)]
+        [DataRow('C', 0x0043)]
+        [DataRow('a', 0x0061)]
+        [DataRow('b', 0x0062)]
+        [DataRow('c', 0x0063)]
+        [DataRow('0', 0x0030)]
+        [DataRow('1', 0x0031)]
+        [DataRow('2', 0x0032)]
+        public void ConvertToUtf32_SomeLetter_ShouldBeExpectedValue(char character, int expectedUtf32Value)
+        {
+            character.ConvertToUtf32().Should().Be(expectedUtf32Value);
+        }
+
+        [TestMethod]
+        [DataRow("A", 0x0041)]
+        [DataRow("B", 0x0042)]
+        [DataRow("C", 0x0043)]
+        [DataRow("a", 0x0061)]
+        [DataRow("b", 0x0062)]
+        [DataRow("c", 0x0063)]
+        [DataRow("0", 0x0030)]
+        [DataRow("1", 0x0031)]
+        [DataRow("2", 0x0032)]
+        public void ConvertToUtf32StringVersion_SomeLetter_ShouldBeExpectedValue(string stringCharacter, int expectedUtf32Value)
+        {
+            stringCharacter.ConvertToUtf32().Should().Be(expectedUtf32Value);
+        }
+
+        [TestMethod]
+        public void GetNumericValue()
+        {
+            char c = '0';
+            c.GetNumericValue();
         }
     }
 }
